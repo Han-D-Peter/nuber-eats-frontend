59b8fb3689a7ccc7e90c41e19c3b203070f0e927
Han-D-Peter <57760806+Han-D-Peter@users.noreply.github.com>
2021-01-22 13:42:46 +0900

    15.7

--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,3 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Login } from "../pages/login";
+import { CreateAccount } from "../pages/create-account";
 
-export const LoggedOutRouter = () => <span>Logged Out</span>;
+export const LoggedOutRouter = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/create-account">
+          <CreateAccount />
+        </Route>
+        <Route path="/">
+          <Login />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
